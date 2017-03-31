a3c0533f29e15e833d647ba107fd272da568fecb
mattofthekidd <mattofthekidd@gmail.com>
2017-03-31 16:37:53 +0000

    Having issues, going to the demo code provided in the Discussion for help.

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatters = [
-    url(r'^hello/',include('hello.urls')),
+    url(r'^hello/', include('hello.urls')),
     url(r'^admin/', admin.site.urls),
-]
+    ]
