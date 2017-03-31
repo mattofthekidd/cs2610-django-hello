e8ba73fc120acdf0c844f7f3edea8ec325077057
mattofthekidd <mattofthekidd@gmail.com>
2017-03-31 17:46:24 +0000

    during class, updated view with pagecount

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
-import PageCount
+from .models import PageCount
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. </br>time: " + time.strftime("%c") + "</br> ")
+    # get row from database
+    row, create = PageCount.objects.get_or_create(page='index')
+    # increment by one
+    row.count += 1
+    # save the value
+    row.save()
+    return HttpResponse("Hello, world. </br>time: " + time.strftime("%c") + "</br>visisted #" + str(row.count)) 
