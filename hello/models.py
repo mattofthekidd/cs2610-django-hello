ac32fde1d09c8694620ccca720cff9af14a12743
mattofthekidd <mattofthekidd@gmail.com>
2017-04-02 18:00:03 +0000

    trying to add a horoscope

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import json
 
 # Create your models here.
 class PageCount(models.Model):
     # page = models.URLField(default='defaultURL')
     page = models.TextField(default='defaultURL')
     count = models.IntegerField(default=0)
+    
+class Horoscope(models.Model):
+    today = models.URLField(default='http://sandipbgt.com/theastrologer/api/horoscope/cancer/today')
+    
+    { "yesterday": "http://sandipbgt.com/theastrologer/api/horoscope/{sunsign}/yesterday", 
+    "sunsign_list": "http://sandipbgt.com/theastrologer/api/sunsigns", 
+    "today": "http://sandipbgt.com/theastrologer/api/horoscope/{sunsign}/today", 
+    "tomorrow": "http://sandipbgt.com/theastrologer/api/horoscope/{sunsign}/tomorrow" }
--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import time
 from .models import PageCount
+from .models import Horoscope
 
 # Create your views here.
 def index(request):
@@ -11,4 +12,13 @@ def index(request):
     row.count += 1
     # save the value
     row.save()
-    return HttpResponse("Hello, world. </br>time: " + time.strftime("%c") + "</br>visisted #" + str(row.count)) 
+    
+    horo, create = Horoscope.objects.get_or_create(today='index')
+    
+    return HttpResponse("Hello, world. </br>time: " + time.strftime("%c") + "</br>visisted #" + str(row.count) + " " + horo) 
+    
+    
+# git clone https://github.com/sandipbgt/theastrologer-api.git
+# cd theastrologer-api
+# pip install -r requirements.txt
+# gunicorn app:app
